9c468ce1fe64b29cd14eea94096cccaa2e48b377
Arina <aarinayamatina2008@gmail.com>
2025-12-09 17:50:33 +0300

    Первое задание

---
+++ kr04/Task1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+public class Student
+{
+    private string name = "Арина";
+    private int age = 20;
+}
+
+class Task1
+{
+    static void Main()
+    {
+        Student student = new Student();
+
+        Type type = typeof(Student);
+
+        FieldInfo nameField = type.GetField("name", 
+            BindingFlags.NonPublic | BindingFlags.Instance);
+
+        string oldName = (string)nameField.GetValue(student);
+        Console.WriteLine($"Старое имя: {oldName}");
+
+        nameField.SetValue(student, "Мария");
+
+        string newName = (string)nameField.GetValue(student);
+        Console.WriteLine($"Новое имя: {newName}");
+    }
+}
